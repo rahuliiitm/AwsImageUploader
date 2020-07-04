43bb6cc5deba1c051b87bf515ffb14ff70168ab0
rahuliiitm <rahul.iiitm06@gmail.com>
2020-07-04 17:35:47 +0530

    AWS login signup functionality

---
+++ ImageUpload.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ImageUpload.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
